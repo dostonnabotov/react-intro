a0ebb3b68f1998dbfb0cdc3c3441393af89f9b49
Doston Nabotov <96492656+dostonnabotov@users.noreply.github.com>
2023-03-04 15:45:57 +0000

    Add useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { createRoot } from "react-dom";
-
-import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <SearchParams />
     </div>
   );
 };
---
+++ src/SearchParams.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            type="text"
+            id="location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
